d454f4fece66b36b18ff01d65f1ff0b781784bf0
Marcin Marciniuk <marcinm222@gmail.com>
2023-10-19 18:36:28 +0200

    bg image changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
 "use client";
 
+import bgImage from "@/public/shirt.png";
+
 import dynamic from "next/dynamic";
 const Canvas = dynamic(() => import("./Canvas"), { ssr: false });
 
 export default function Home() {
   return (
-    <div className="w-full h-[600px] bg-[url(/shirt.png)] bg-no-repeat bg-center">
+    <div
+      className="w-full h-[600px] bg-no-repeat bg-center"
+      style={{ backgroundImage: `url(${bgImage.src})` }}
+    >
       <Canvas />
     </div>
   );
